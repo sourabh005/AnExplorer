e231fb54b8edac87cd9e43994fb6aefdcbd53566
Sourabh Thakur <thakursourabh272@gmail.com>
2018-02-10 14:23:43 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -11,22 +11,16 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
 LOCAL_PATH:= $(call my-dir)
 
+#AnExplorer
 include $(CLEAR_VARS)
-
 LOCAL_MODULE_TAGS := optional
-
 LOCAL_SRC_FILES := AnExplorer.apk
-
 LOCAL_MODULE := AnExplorer
-
 LOCAL_JACK_ENABLED := disabled
-
 LOCAL_CERTIFICATE := platform
-
 LOCAL_MODULE_CLASS := APPS
-
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-
 include $(BUILD_PREBUILT)
